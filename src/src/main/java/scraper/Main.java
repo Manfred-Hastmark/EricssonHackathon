0b68e0c767eccc85253c261df3fd8af661bf806b
Manfred Hästmark <hastmark@student.chalmers.se>
2021-11-12 15:20:16 +0100

    switching to javascript

--- src/src/main/java/scraper/Main.java
+++ src/src/main/java/scraper/Main.java
@@ -1,8 +1,13 @@
 package sample;
 
+import com.google.common.net.PercentEscaper;
+import scraper.Book;
+import org.apache.commons.io.FileUtils;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 
@@ -10,6 +15,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        getBookBySearch("Linjär algebra");
     }
 
     public static void getLunch(URL url){
@@ -22,14 +28,45 @@ public class Main {
         }
     }
 
-    public static double getValueAt(String pattern, StringBuilder page) {
+    public static Book getBookBySearch(String search) {
+        try {
+            //Setup connection to site
+            search = search.replace(" ", "%20");
+            search = search.replace("ä", "%C3%A4");
+
+
+
+            URL url = new URL("https://www.chalmersstore.se/sok/" + search);
+            url.
+            File file = new File("temp.txt");
+            FileUtils.copyURLToFile(url, file);
+            URLConnection connection = url.openConnection();
+
+            //Get page as stringbuilder
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder page = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                page.append(inputLine);
+            in.close();
+
+
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+
+
+        /*
         int index = page.indexOf(pattern) + pattern.length();
         StringBuilder result = new StringBuilder();
         while (44 < page.charAt(index) && page.charAt(index) < 58 && page.charAt(index) != 47) { //Number 0-9
             result.append(page.charAt(index));
             index++;
         }
-        return Double.parseDouble(result.toString().replace(",", "."));
+        return Double.parseDouble(result.toString().replace(",", "."));*/
+        return null;
     }
 
 
