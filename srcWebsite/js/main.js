163635a4336290c85832e4d46cfbfc3f593cb4d7
mattiasdjarv <djarv.mattias@gmail.com>
2021-11-12 16:47:34 +0100

    Update main.js

--- srcWebsite/js/main.js
+++ srcWebsite/js/main.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-
-
-function showInput() {
-  document.getElementById('display').innerHTML = document.getElementById("query").value;
-  var query = document.getElementById("query").value;
-  
-  scrape();
-}
-=======
 var userSearch;
     function showInput() {
         document.getElementById('display').innerHTML = 
                     document.getElementById("query").value;
 		userSearch = document.getElementById("query").value;
     }
->>>>>>> 39654781c61c78b7095ceb7571a9a28795dca927
+	
+	function newBook() {
+		document.getElementById('formDiv').style.display = "block";
+		document.getElementById('searchDiv').style.display = "none";
+	}
+	
