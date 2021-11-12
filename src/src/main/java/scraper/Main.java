1fd96ad7a9cd31e76ff45e9536cf7b1341c78fdb
Manfred Hästmark <hastmark@student.chalmers.se>
2021-11-12 23:41:04 +0100

    systemet

--- src/src/main/java/scraper/Main.java
+++ src/src/main/java/scraper/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import com.google.common.net.PercentEscaper;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import scraper.Book;
 import org.apache.commons.io.FileUtils;
 
@@ -10,12 +11,93 @@ import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
+
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        getBookBySearch("Linjär algebra");
+
+    public Main() throws Exception{
+        System.setProperty("webdriver.gecko.driver","C:/Program Files/geckodriver.exe");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("marionette",true);
+        WebDriver driver= new FirefoxDriver(); driver.get("https://www.systembolaget.se/sok/?categoryLevel1=%C3%96l");
+        driver.findElement(By.xpath("/html/body/div[4]/div/div/div/div/section/div/div/div[4]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[4]/div/div/div/div/div/div[2]/div[2]/button[2]")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[1]/div/button[2]")).click();
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div/form/label/div/input")).sendKeys("Kapell");
+        TimeUnit.MILLISECONDS.sleep(200);
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div/form/label/div/input")).sendKeys("p");
+        TimeUnit.MILLISECONDS.sleep(800);
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/div/div/div/form/label/div/div/div/ul/li/div")).click();
+
+        int antalSidor = 4;
+        for(int i = 0; i < antalSidor; i++){
+            TimeUnit.MILLISECONDS.sleep(1000);
+            driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[" + (32 + i*31) + "]/div[1]/button")).click();
+
+        }
+        TimeUnit.MILLISECONDS.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[155]/div[1]/button")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[185]/div[1]/button")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[215]/div[1]/button")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[245]/div[1]/button")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[275]/div[1]/button")).click();
+        /*
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[32]/div[1]/button")).click();
+        TimeUnit.SECONDS.sleep(1);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[63]/div[1]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[94]/div[1]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[155]/div[1]/button")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[185]/div[1]/button")).click();
+        //driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]/div[245]/div[1]/button")).click();
+        String url = driver.getCurrentUrl();*/
+
+        //driver.close();
+    }
+
+    public class Beer {
+        final private String name;
+        final private int volume;
+        final private int price;
+        final private double apk;
+
+        public Beer(String name, int volume, int price) {
+            this.name = name;
+            this.volume = volume;
+            this.price = price;
+            this.apk = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getVolume() {
+            return volume;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public double getApk() {
+            return apk;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
     }
 
     public static void getLunch(URL url){
@@ -37,7 +119,6 @@ public class Main {
 
 
             URL url = new URL("https://www.chalmersstore.se/sok/" + search);
-            url.
             File file = new File("temp.txt");
             FileUtils.copyURLToFile(url, file);
             URLConnection connection = url.openConnection();
--- srcWebsite/js/main.js
+++ srcWebsite/js/main.js
@@ -1,12 +1,40 @@
+
 var userSearch;
-    function showInput() {
-        document.getElementById('display').innerHTML = 
-                    document.getElementById("query").value;
-		userSearch = document.getElementById("query").value;
-    }
-	
-	function newBook() {
-		document.getElementById('formDiv').style.display = "block";
-		document.getElementById('searchDiv').style.display = "none";
-	}
+
+
+
+const puppet = require('puppeteer');
+
+async function scrapeProduct(url){
+    const browser = await puppet.launch();
+    const page = await browser.newPage();
+    await  page.goto(url);
+
+    const[el] = await page.$x('/html/body/div[4]/div/div[1]/div/div/div[2]/div/div/div/div[3]/div[1]/div/div/tws-util-heading/div/h2/a');
+    const src = await el.getProperty('src');
+    const srcText = await src.jsonValue();
+
+    const[el2] = await page.$x('/html/body/div[4]/div/div[1]/div/div/div[2]/div/div/div/div[3]/div[1]/div/div/tws-util-heading/div/h2/a');
+    const txt = await el2.getProperty('textContent');
+    const rawText = await txt.jsonValue();
+    console.log(srcText, rawText);
+
+}
+
+scrapeProduct('https://www.chalmersstore.se/sok/python%22');
+
+function showInput() {
+	document.getElementById('display').innerHTML = 
+	document.getElementById("query").value;
+	userSearch = document.getElementById("query").value;
+}
+
+function newBook() {
+	document.getElementById('formDiv').style.display = "block";
+	document.getElementById('searchDiv').style.display = "none";
+}
+
+
+
+
 	
